e69ddd8c645abdd362cffff6ead3571cc457d15b
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2021-03-29 19:54:08 -0400

    remove some misc code that doesn't need to be on the HPC

--- code/MakePrettierCombinedVegMap.R
+++
@@ -1,8 +0,0 @@
-#prettier combined vegetation map of Finger Lakes. Couldn't get this to work in markdown
-
-rm(list=ls())
-
-veglayer <- 'evt'
-load(paste0('./data/CombinedVegCDLRasters/combined', veglayer, 'raster.RDA'))
-rasterVis::levelplot(combined, att='Class_Name')
-
