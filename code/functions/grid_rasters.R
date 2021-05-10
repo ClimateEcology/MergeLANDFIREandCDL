5d46c2291d1d9bb4a1d9133a7515e9d40f7a95a5
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2021-05-10 15:17:18 -0400

    re-factor tile gridding function to simplify

--- code/functions/grid_rasters.R
+++ code/functions/grid_rasters.R
@@ -1,39 +1,48 @@
 # function to split CDL and LANDFIRE regional or national rasters into gridded tiles
 
-grid_rasters <- function(rpath, CDLYear, regionalextent=NA, 
+grid_rasters <- function(rpath, regionalextent=NA, 
                         tile_dir, div, buffercells=c(0,0),
-                        window_size = NA, skipNAtiles = T, NAvalues, 
-                        writetiles = T) {
+                        skipNAtiles = T, NAvalue, 
+                        writetiles = T, ...) {
   
-  #separate file paths to CDL and vegetation rasters
+  ######################################################################################################
+  ##### Part 1: Setup and load data
+  
+  # lOad libraries
+  library(future); library(foreach)
+  
+  #separate file paths to CDL and vegetation rasters. 
   cdl_path <- rpath[1]
   
+  # if only one raster path is supplied, set veg_path to NA
   if (length(rpath > 1)) {
     veg_path <- rpath[2]
   } else {
     veg_path <- NA
   }
   
-  #set up logging
+  #set up logger to write status updates
   library(logger)
   logger::log_threshold(DEBUG)
   
-  # load CDL raster, will use this to re-project state polygons to the same projection
+  # load raster1
+  # We will use raster object to re-project state polygons
   cdl <- raster::raster(cdl_path)
   
-  # read or download shapefile of US states
-  # then, re-project to match CDL raster
+  # if necessary, download polygon layer of state boundaries
   if (!any(is.na(regionalextent)) & is.character(regionalextent)) {
     logger::log_info('Re-projecting regional shapefile to match CDL raster.')
     
+    # download shapefile of US states
     region <- tigris::states() %>% dplyr::filter(NAME %in% regionalextent) %>%
-      sf::st_transform(crs = sf::st_crs(cdl)) 
+      sf::st_transform(crs = sf::st_crs(cdl)) # re-project polygon layer to match raster1
     
   } else if ('sf' %in% class(regionalextent)) {
     region <- sf::st_transform(regionalextent, crs = sf::st_crs(cdl))
   }
   
-  ##### Split CDL and LANDFIRE vegetation rasters into tiles
+  ######################################################################################################
+  ##### Part 2: Crop national raster(s) to regional extent
   
   # create directory for output files if it doesn't already exist
   if (!dir.exists(tile_dir)) {
@@ -42,25 +51,29 @@ grid_rasters <- function(rpath, CDLYear, regionalextent=NA,
   
   
   # read input raster and crop to extent of provided shapefile
-  # here I use the terra package because it is faster than raster. 
-  # We need to convert to a raster object so the SpaDES package works
+  # use the terra package because it is faster than raster. 
   if (!any(is.na(regionalextent))) {
     
     tictoc::tic()
-    logger::log_info('Cropping national rasters to shapefile extent (if regionalextent is provided).')
+    logger::log_info('Cropping national raster(s) to shapefile extent (if regionalextent is provided).')
     
     region_cdl <- terra::rast(cdl_path) %>%
       terra::crop(y=region) %>%
-      raster::raster()
+      raster::raster()   # convert to a raster object so the SpaDES package works
+
     
     if (!is.na(veg_path)) {
       region_nvc <- terra::rast(nvc_path) %>%
         terra::crop(y=region) %>%
-        raster::raster() 
+        raster::raster()   # convert to a raster object so the SpaDES package works
+
     }
     
       tictoc::toc()
   }
+  
+  ######################################################################################################
+  ##### Part 3: Split raster1 into tiles 
   
   logger::log_info('Splitting regional raster into specified number of tiles (n = xdiv * ydiv).')
   
@@ -70,17 +83,21 @@ grid_rasters <- function(rpath, CDLYear, regionalextent=NA,
   
   tictoc::tic()
   
-  # Split CDL and LANDFIRE rasters into chunks using a regular grid
-  cdl_tiles <- SpaDES.tools::splitRaster(r=region_cdl, nx=div[1], ny=div[2], buffer=buffercells,
-                                         cl=cl)
-
+  # split raster1 into tiles using a regular grid
+  cdl_tiles <- SpaDES.tools::splitRaster(r=region_cdl, nx=div[1], ny=div[2], 
+                                         buffer=buffercells, cl=cl)
+  
+  ######################################################################################################
+  ##### Part 4: If raster2 file path is provided, split raster2 into tiles
+  
   if (!is.na(veg_path)) {
-    nvc_tiles <- SpaDES.tools::splitRaster(r=region_nvc, nx=div[1], ny=div[2], buffer=buffercells,
-                                           cl=cl)
-  }
+    nvc_tiles <- SpaDES.tools::splitRaster(r=region_nvc, nx=div[1], ny=div[2], 
+                                         buffer=buffercells, cl=cl)
   tictoc::toc()
   
-  ##### Skip background tiles that are all NA (if skipNAtiles == T)
+  
+  ######################################################################################################
+  ##### Part 5: Identify background tiles that are all NA (if skipNAtiles == T)
   
   if (skipNAtiles == T) {
     
@@ -92,16 +109,18 @@ grid_rasters <- function(rpath, CDLYear, regionalextent=NA,
     future::plan(multisession)
     
     tictoc::tic()
+    # list of tiles to skip for raster1
     todiscard_cdl <- furrr::future_map(.x=cdl_tiles, .f = function(x) {
-      raster::cellStats(x, stat=max) == NAvalues[1] },
+      raster::cellStats(x, stat=max) == NAvalue[1] },
       .options = furrr::furrr_options(seed = TRUE)) %>% unlist()
+    # list of tiles to skip for raster2
     todiscard_nvc <- furrr::future_map(.x=nvc_tiles, .f = function(x) {
-      raster::cellStats(x, stat=max) == NAvalues[2] },
+      raster::cellStats(x, stat=max) == NAvalue[2] },
       .options = furrr::furrr_options(seed = TRUE)) %>% unlist()
     tictoc::toc()
 
     
-    # if NA tiles from CDL and NVC perfectly match, discard them and keep the rest
+    # if NA tiles from raster1 and raster2 perfectly match, discard them and keep the rest
     if (all(todiscard_cdl == todiscard_nvc)) {
       
       input_rasters <- furrr::future_map2(.x= purrr::discard(cdl_tiles, todiscard_nvc), 
@@ -109,10 +128,10 @@ grid_rasters <- function(rpath, CDLYear, regionalextent=NA,
       
     } else if (any(todiscard_cdl != todiscard_nvc)) {
       
-      # if NA tiles from CDL and NVC do NOT totally match, only discard tiles that are all NA in BOTH layers
+      # if NA tiles from raster1 and raster2 do NOT totally match, only discard tiles that are all NA in BOTH layers
       
-      warning('NVC and CDL tiles that are all background do not match. 
-           The boundaries of NASS CDL and LANDFIRE NVC rasters are probably different.
+      warning('Raster1 and Raster2 100% background tiles do not match. 
+           The boundaries (e.g. land/water) of these rasters are probably different.
            Look at the tiles in the mismatched tiles folder.')
       
       input_rasters <- furrr::future_map2(.x= purrr::discard(cdl_tiles, todiscard_nvc&todiscard_cdl), 
@@ -127,10 +146,12 @@ grid_rasters <- function(rpath, CDLYear, regionalextent=NA,
       # (no match = one layer is all background, but the other has values other than NA)
       for (i in which(todiscard_cdl != todiscard_nvc)) {
 
-        writeRaster(cdl_tiles[[i]], paste0(tile_dir, "/MismatchedBorderTiles/CDLTile", i, ".tif"), overwrite=T)
+        raster::writeRaster(cdl_tiles[[i]], paste0(tile_dir, "/MismatchedBorderTiles/CDL", 
+                                                   CDLYear, "Tile", i, ".tif"), overwrite=T)
         
         if (!is.na(veg_path)) {
-          writeRaster(nvc_tiles[[i]], paste0(tile_dir, "/MismatchedBorderTiles/NVCTile", i, ".tif"), overwrite=T)
+          raster::writeRaster(nvc_tiles[[i]], paste0(tile_dir, "/MismatchedBorderTiles/NVCTile", 
+                                                   i, ".tif"), overwrite=T)
         }
       }
       
@@ -140,7 +161,7 @@ grid_rasters <- function(rpath, CDLYear, regionalextent=NA,
         logger::log_info('Writing output tiles.')
         
         # set up parallel processing cluster
-        cl <- parallel::makeCluster(detectCores() - 2)  # use all but 2 cores
+        cl <- parallel::makeCluster(parallel::detectCores() - 2)  # use all but 2 cores
         parallel::clusterExport(cl=cl, envir=environment(), varlist=c('region_cdl', 'region_nvc'))
         doParallel::registerDoParallel(cl)  # register the parallel backend
         
@@ -153,7 +174,7 @@ grid_rasters <- function(rpath, CDLYear, regionalextent=NA,
             dir.create(paste0(tile_dir, "/NVC"))
           }
           
-          raster::writeRaster(cdl_tiles[[i]], paste0(tile_dir, "/CDL", CDLYear, "/CDLTile", i, ".tif"), overwrite=T)
+          raster::writeRaster(cdl_tiles[[i]], paste0(tile_dir, "/CDL/CDL", CDLYear, "Tile", i, ".tif"), overwrite=T)
           
           if (!is.na(veg_path)) {
             raster::writeRaster(nvc_tiles[[i]], paste0(tile_dir, "/NVC/NVCTile", i, ".tif"), overwrite=T)
@@ -169,7 +190,7 @@ grid_rasters <- function(rpath, CDLYear, regionalextent=NA,
     #turn on parallel processing for furrr package
     future::plan(multisession)
     
-    # convert raster tiles to list (each list element has 2 rasters)
+    # convert raster tiles to a list (each list element has 2 rasters)
     input_rasters <- furrr::future_map2(.x= cdl_tiles, .y= nvc_tiles, .f=c)
     
     }
@@ -179,7 +200,7 @@ grid_rasters <- function(rpath, CDLYear, regionalextent=NA,
       logger::log_info('Writing output tiles.')
       
       # set up parallel processing cluster
-      cl <- parallel::makeCluster(detectCores() - 2)  # use all but 2 cores
+      cl <- parallel::makeCluster(parallel::detectCores() - 2)  # use all but 2 cores
       parallel::clusterExport(cl=cl, envir=environment(), varlist=c('region_cdl', 'region_nvc'))
       doParallel::registerDoParallel(cl)  # register the parallel backend
   
@@ -202,9 +223,16 @@ grid_rasters <- function(rpath, CDLYear, regionalextent=NA,
     }
   }
   
-  stopCluster(cl); future::plan(sequential)
+  parallel::stopCluster(cl); future::plan(sequential)
   
-  logger::log_info('Grid function complete, returning pairs of rasters as a list.')
+  logger::log_info('Gridding function complete, returning pairs of raster tiles as a list.')
   
   return(input_rasters)
+  
+  } else if (is.na(veg_path)) {
+    
+    logger::log_info('Gridding function complete, returning raster tiles as a list.')
+    return(cdl_tiles)
+  }
+  
 }
