de465997656ce8b611a51b9c2cd9b087ac216202
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2021-07-08 15:36:54 -0400

    tweak allstate mosaic code to specify CDL year

--- code/MosaicStateRasters.R
+++ code/MosaicStateRasters.R
@@ -1,16 +1,19 @@
 rm(list=ls())
 
-library(dplyr)
+library(dplyr); library(terra)
+CDLYear <- 2016
+
 # save list of directories w/ finished state maps
 is_tile_dir <- list.dirs('./data', full.names = T, recursive = F) %>%
   stringr::str_detect(pattern='(..)Tiles')
 
 dirs_tomerge <- list.dirs('./data', full.names = T, recursive = F)[is_tile_dir]
 
-# save list of file paths to merged rasters
+# save list of file paths to state rasters
 for (i in 1:length(dirs_tomerge)) {
   temp <- list.files(dirs_tomerge[i], full.names = T)
-  oneraster <- temp[grepl(temp, pattern='Final')]
+  oneraster <- temp[grepl(temp, pattern='Final') & 
+    grepl(temp, pattern=as.character(get('CDLYear')))] # only process rasters that match the CDL year specified above
   
   if (i == 1) {
     allrasters <- oneraster
@@ -18,6 +21,7 @@ for (i in 1:length(dirs_tomerge)) {
     allrasters <- c(allrasters, oneraster)
   }
 }
+
 # make list of actual raster objects (not file paths)
 allstates <- vector("list", length(allrasters))
 
@@ -25,4 +29,5 @@ for (i in 1:length(allrasters)) {
   allstates[[i]] <- terra::rast(allrasters[i])
 }
 
-allstates_map <- terra::mosaic(allstates, fun='mean')
+# execute mosaic function
+allstates_map <- rlang::exec("mosaic", !!!allstates, fun='mean', filename=paste0('./data/NationalMergedRaster', CDLYear, '.tif'), overwrite=T)
