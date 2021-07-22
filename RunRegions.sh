2d2c7473f54fbd0465dbeb254e60ea59aa726ddc
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2021-07-22 15:14:49 -0400

    fix file name

--- RunRegions.sh
+++ RunRegions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 year=2016
-sbatch --job-name=MergeSoutheast --export=ALL,cdlyear=$year,region='Southeast' RunMergeLANDFIREandCDL_byState.sbatch
-sbatch --job-name=MergeNortheast --export=ALL,cdlyear=$year,region='Northeast' RunMergeLANDFIREandCDL_byState.sbatch
-sbatch --job-name=MergeMidwest --export=ALL,cdlyear=$year,region='Midwest' RunMergeLANDFIREandCDL_byState.sbatch
-sbatch --job-name=MergeWest --export=ALL,cdlyear=$year,region='West' RunMergeLANDFIREandCDL_byState.sbatch
+sbatch --job-name=MergeSoutheast --export=ALL,cdlyear=$year,region='Southeast' RunMerge_in_container_bigmem_bystate.sbatch
+sbatch --job-name=MergeNortheast --export=ALL,cdlyear=$year,region='Northeast' RunMerge_in_container_bigmem_bystate.sbatch
+sbatch --job-name=MergeMidwest --export=ALL,cdlyear=$year,region='Midwest' RunMerge_in_container_bigmem_bystate.sbatch
+sbatch --job-name=MergeWest --export=ALL,cdlyear=$year,region='West' RunMerge_in_container_bigmem_bystate.sbatch
