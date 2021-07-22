7c6a303eaeb40ebf70b0279308582d1d48689a84
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2021-07-22 12:57:58 -0400

    try again

--- code/TestCommandArgs.R
+++ code/TestCommandArgs.R
@@ -6,11 +6,12 @@ source('./code/functions/grid_rasters.R')
 
 library(dplyr);  library(raster); library(sf); library(logger); library(future)
 
-args <- commandArgs(trailingOnly = T)
+args <- commandArgs(trailingOnly = F)
 
 # specify input parameters
 logger::log_info(paste0('arg 1 is ', args[1]))
-logger::log_info(paste0('arg 1 is ', args[2]))
+logger::log_info(paste0('arg 2 is ', args[2]))
+logger::log_info(paste0('arg 3 is ', args[3]))
 
 
 CDLYear <- args[2] # year of NASS Cropland Data Layer
