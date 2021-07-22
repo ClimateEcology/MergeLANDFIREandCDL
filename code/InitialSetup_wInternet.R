186c6a10bc66182eb2284b9c744690e647ba7722
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2021-07-22 09:50:57 -0400

    initial setup to run country in 4 regions

--- code/InitialSetup_wInternet.R
+++ code/InitialSetup_wInternet.R
@@ -4,33 +4,42 @@
 library(dplyr); library(sf)
 
 bystate <- T
-wholecountry <- T
-regionName <- 'National'
+wholecountry <- F
+regionName <- 'Northeast'
 
-states <- c('West Virginia', 'Pennsylvania', 'Maryland',
-                    'Delaware', 'New Jersey', 'New York', 'New Hampshire', 'Vermont', 'Maine', 'Connecticut',
-                    'Massachusetts', 'Rhode Island') # list of states within region
+# states <- c('West Virginia', 'Pennsylvania', 'Maryland',
+#                     'Delaware', 'New Jersey', 'New York', 'New Hampshire', 'Vermont', 'Maine', 'Connecticut',
+#                     'Massachusetts', 'Rhode Island', 'District of Columbia') # list of states within region
+
+national <- tigris::states() %>% sf::st_as_sf() %>%
+  dplyr::filter(!NAME %in% c('Alaska', 'American Samoa', 'Commonwealth of the Northern Mariana Islands', 
+                             'Guam', 'Hawaii', 'Puerto Rico', 'United States Virgin Islands'))
+
+if (regionName == 'Northeast') {
+  states <- national$NAME[national$region == 1]
+} else if (regionName == 'Southeast') {
+  states <- national$NAME[national$region == 2]
+} else if (regionName == 'MidWest') {
+  states <- national$NAME[national$region == 3]
+} else if (regionName == 'West') {
+  states <- national$NAME[national$region == 4]
+}
 
 
 if (bystate == T & wholecountry == F) {
   # download shapefile of US states
-  region <- tigris::states() %>% sf::st_as_sf() %>%
-    dplyr::filter(NAME %in% states) # filter to only selected states
+  region <- dplyr::filter(national, NAME %in% states) # filter to only selected states
   
   sf::st_write(region, paste0('./data/SpatialData/', regionName, '.shp'), append=F)
   
 } else if (bystate == T & wholecountry == T) {
   # download shapefile of US states
-  region <- tigris::states() %>% sf::st_as_sf() %>%
-    dplyr::filter(!NAME %in% c('Alaska', 'American Samoa', 'Commonwealth of the Northern Mariana Islands', 
-                               'Guam', 'Hawaii', 'Puerto Rico', 'United States Virgin Islands'))
-  
+  region <- national
   sf::st_write(region, paste0('./data/SpatialData/', regionName, '.shp'), append=F)
   
 } else if (bystate == F) {
   # download shapefile of US states
-  region <- tigris::states() %>% sf::st_as_sf() %>%
-    dplyr::filter(NAME %in% states) %>% # filter to only selected states
+  region <- dplyr::filter(national, NAME %in% states) %>% # filter to only selected states
     sf::st_union()  
   
   sf::st_write(region, paste0('./data/SpatialData/', regionName, '_OnePoly.shp'), append=F)
