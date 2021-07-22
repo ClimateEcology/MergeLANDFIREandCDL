374976013fa027f5e7bc0a860ffc5d3eb601fc2f
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2021-07-22 12:56:43 -0400

    args aren't working. troubleshoot

---
+++ code/TestCommandArgs.R
@@ -0,0 +1,17 @@
+rm(list=ls())
+
+# import function to merge together CDL and LANDFIRE tiles
+source('./code/functions/merge_landfire_cdl_4tiles.R')
+source('./code/functions/grid_rasters.R')
+
+library(dplyr);  library(raster); library(sf); library(logger); library(future)
+
+args <- commandArgs(trailingOnly = T)
+
+# specify input parameters
+logger::log_info(paste0('arg 1 is ', args[1]))
+logger::log_info(paste0('arg 1 is ', args[2]))
+
+
+CDLYear <- args[2] # year of NASS Cropland Data Layer
+regionName <- args[2] # region to process
