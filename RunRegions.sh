b6adf7392d453048f0605c9dd129da3c6d49f3a1
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2021-08-11 15:06:13 -0400

    for W Texas, make tiles

--- RunRegions.sh
+++ RunRegions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 year=2016
-tiles=FALSE
-merge=FALSE
+tiles=TRUE
+merge=TRUE
 sbatch --job-name=MergeSoutheast --export=ALL,cdlyear=$year,region='Southeast',mktiles=$tiles,runmerge=$merge RunMerge_in_container_bigmem_bystate.sbatch
 #sbatch --job-name=MergeNortheast --export=ALL,cdlyear=$year,region='Northeast',mktiles=$tiles,runmerge=$merge RunMerge_in_container_bigmem_bystate.sbatch
 #sbatch --job-name=MergeMidwest --export=ALL,cdlyear=$year,region='Midwest',mktiles=$tiles,runmerge=$merge RunMerge_in_container_bigmem_bystate.sbatch
