1601eed1385341c55c1bb9fe7292356c7e4cd40c
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2021-08-13 11:19:37 -0400

    take out unnecessary IDstrings

--- code/MosaicStateRasters.R
+++ code/MosaicStateRasters.R
@@ -15,11 +15,6 @@ if (laptop == T) {
 # Stitch together merged state rasters
 # provide all CDL year we might possibly want. Mosaic function will skip those with no available rasters.
 for (CDLYear in c(2012:2020)) { 
-  for (index in c('Forage', 'Insecticide')) {
-    
     mosaic_state_rasters(CDLYear=CDLYear, parentdir=intermediate_dir,
-      tilestring=tilestring, IDstring1=index,
-      IDstring2=distance, season=season, outdir=outdir)
-    
-  }
+      tilestring=tilestring, outdir=outdir)
 }
