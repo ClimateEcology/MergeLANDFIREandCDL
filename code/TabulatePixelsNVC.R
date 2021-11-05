8520de597935d3c9f9ebfd98d908b006920507f9
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2021-11-05 10:44:18 -0400

    run all state pixel freq

--- code/TabulatePixelsNVC.R
+++ code/TabulatePixelsNVC.R
@@ -5,7 +5,7 @@ nvc <- raster::raster('./data/SpatialData/LANDFIRE/US_200NVC/Tif/us_200nvc.tif')
 states <- sf::st_read('./data/SpatialData/us_states_better_coasts.shp') %>%
   sf::st_transform(crs = sf::st_crs(nvc))
 
-for (state in unique(states$STATE)[1:4]) {
+for (state in unique(states$STATE)) {
   
   one_state <- dplyr::filter(states, STATE==state) #%>%
     #sf::st_combine()
