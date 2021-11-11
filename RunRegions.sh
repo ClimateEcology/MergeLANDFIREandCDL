462b2877f4eff52540ccbe0572f919a62827a3bd
melaniekamm <melanie.a.kammerer@gmail.com>
2021-11-11 13:20:51 -0600

    small changes

--- RunRegions.sh
+++ RunRegions.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-year=2016
+year=2019
 tiles=TRUE
 merge=TRUE
-#sbatch --job-name=MergeSoutheast --export=ALL,cdlyear=$year,region='Southeast',mktiles=$tiles,runmerge=$merge RunMerge_in_container_bigmem_bystate.sbatch
+sbatch --job-name=MergeSoutheast --export=ALL,cdlyear=$year,region='Southeast',mktiles=$tiles,runmerge=$merge RunMerge_in_container_bigmem_bystate.sbatch
 sleep 1s
-#sbatch --job-name=MergeNortheast --export=ALL,cdlyear=$year,region='Northeast',mktiles=$tiles,runmerge=$merge RunMerge_in_container_bigmem_bystate.sbatch
+sbatch --job-name=MergeNortheast --export=ALL,cdlyear=$year,region='Northeast',mktiles=$tiles,runmerge=$merge RunMerge_in_container_bigmem_bystate.sbatch
 sleep 1s
 sbatch --job-name=MergeMidwest --export=ALL,cdlyear=$year,region='Midwest',mktiles=$tiles,runmerge=$merge RunMerge_in_container_bigmem_bystate.sbatch
 sleep 1s
-#sbatch --job-name=MergeWest --export=ALL,cdlyear=$year,region='West',mktiles=$tiles,runmerge=$merge RunMerge_in_container_bigmem_bystate.sbatch
+sbatch --job-name=MergeWest --export=ALL,cdlyear=$year,region='West',mktiles=$tiles,runmerge=$merge RunMerge_in_container_bigmem_bystate.sbatch
