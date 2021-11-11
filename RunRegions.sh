405bcfb68c30cfdb0f927b17d939730a9b9b644a
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2021-11-11 14:20:13 -0500

    add sbatch to process large states with fewer cpus, re-run Texas for all years

--- RunRegions.sh
+++ RunRegions.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-year=2016
 tiles=TRUE
 merge=TRUE
-#sbatch --job-name=MergeSoutheast --export=ALL,cdlyear=$year,region='Southeast',mktiles=$tiles,runmerge=$merge RunMerge_in_container_bigmem_bystate.sbatch
+allstates=FALSE
+
+year=2016
+sbatch --job-name=MergeSoutheast --export=ALL,cdlyear=$year,region='Southeast',\
+mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_large_states.sbatch
 sleep 1s
-#sbatch --job-name=MergeNortheast --export=ALL,cdlyear=$year,region='Northeast',mktiles=$tiles,runmerge=$merge RunMerge_in_container_bigmem_bystate.sbatch
+
+sbatch --job-name=MergeNortheast --export=ALL,cdlyear=$year,region='Northeast',\
+mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
 sleep 1s
-sbatch --job-name=MergeMidwest --export=ALL,cdlyear=$year,region='Midwest',mktiles=$tiles,runmerge=$merge RunMerge_in_container_bigmem_bystate.sbatch
+
+sbatch --job-name=MergeMidwest --export=ALL,cdlyear=$year,region='Midwest',\
+mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
 sleep 1s
-#sbatch --job-name=MergeWest --export=ALL,cdlyear=$year,region='West',mktiles=$tiles,runmerge=$merge RunMerge_in_container_bigmem_bystate.sbatch
+
+sbatch --job-name=MergeWest --export=ALL,cdlyear=$year,region='West',\
+mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_large_states.sbatch
