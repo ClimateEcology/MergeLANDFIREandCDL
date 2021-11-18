cfb237c0941a92dc752dda99e8c5795f314fed52
melaniekamm <melanie.a.kammerer@gmail.com>
2021-11-18 14:05:45 -0600

    try Texas with fewer cores

--- RunRegions.sh
+++ RunRegions.sh
@@ -2,9 +2,9 @@
 
 tiles=TRUE
 merge=TRUE
-allstates=FALSE
+allstates=TRUE
 
-year=2016
+year=2019
 sbatch --job-name=MergeSoutheast --export=ALL,cdlyear=$year,region='Southeast',\
 mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_large_states.sbatch
 sleep 1s
