4d253a674d59170c7a72967885b33e4d57ee0de0
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2021-12-15 10:20:24 -0500

    try using large states sbatch

--- RunTexas.sh
+++ RunTexas.sh
@@ -6,20 +6,20 @@ allstates=FALSE
 
 year=2016
 sbatch --job-name=Texas2016 --export=ALL,cdlyear=$year,region='Southeast',\
-mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
+mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_large_states.sbatch
 sleep 1s
 
 year=2017
 sbatch --job-name=Texas2017 --export=ALL,cdlyear=$year,region='Southeast',\
-mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
+mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_large_states.sbatch
 sleep 1s
 
 year=2018
 sbatch --job-name=Texas2018 --export=ALL,cdlyear=$year,region='Southeast',\
-mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
+mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_large_states.sbatch
 sleep 1s
 
 year=2019
 sbatch --job-name=Texas2018 --export=ALL,cdlyear=$year,region='Southeast',\
-mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
+mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_large_states.sbatch
 sleep 1s
