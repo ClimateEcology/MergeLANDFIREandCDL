ff74b8fe8e4ffb63915b1e11c780d140a4f56d08
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2021-12-15 12:38:24 -0500

    fix job name typo

--- RunTexas.sh
+++ RunTexas.sh
@@ -20,6 +20,6 @@ mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_large_states.sbatch
 sleep 1s
 
 year=2019
-sbatch --job-name=Texas2018 --export=ALL,cdlyear=$year,region='Southeast',\
+sbatch --job-name=Texas2019 --export=ALL,cdlyear=$year,region='Southeast',\
 mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_large_states.sbatch
 sleep 1s
