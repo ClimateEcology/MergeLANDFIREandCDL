eb13ee15a4d75c821b0325e931102e5164929dee
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2021-12-15 15:11:19 -0500

    try again to request exclusive bigmem nodes

--- RunTexas.sh
+++ RunTexas.sh
@@ -4,22 +4,22 @@ tiles=TRUE
 merge=TRUE
 allstates=FALSE
 
-#year=2016
-#sbatch --job-name=Texas2016 --export=ALL,cdlyear=$year,region='Southeast',\
-#mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_large_states.sbatch
-#sleep 1s
-
-#year=2017
-#sbatch --job-name=Texas2017 --export=ALL,cdlyear=$year,region='Southeast',\
-#mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_large_states.sbatch
-#sleep 1s
-
-year=2018
-sbatch --job-name=Texas2018 --export=ALL,cdlyear=$year,region='Southeast',\
+year=2016
+sbatch --job-name=Texas2016 --export=ALL,cdlyear=$year,region='Southeast',\
 mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_large_states.sbatch
 sleep 1s
 
-#year=2019
-#sbatch --job-name=Texas2019 --export=ALL,cdlyear=$year,region='Southeast',\
+year=2017
+sbatch --job-name=Texas2017 --export=ALL,cdlyear=$year,region='Southeast',\
+mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_large_states.sbatch
+sleep 1s
+
+#year=2018
+#sbatch --job-name=Texas2018 --export=ALL,cdlyear=$year,region='Southeast',\
 #mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_large_states.sbatch
 #sleep 1s
+
+year=2019
+sbatch --job-name=Texas2019 --export=ALL,cdlyear=$year,region='Southeast',\
+mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_large_states.sbatch
+sleep 1s
