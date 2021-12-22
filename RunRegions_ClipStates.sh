8188a5871f4da97e0d70eb6219ae099ed2c68891
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2021-12-22 16:42:47 -0500

    run only 2017

--- RunRegions_ClipStates.sh
+++ RunRegions_ClipStates.sh
@@ -8,13 +8,13 @@ sbatch --job-name=ClipSoutheast --export=ALL,cdlyear=$year,region='Southeast',\
 clipstates=$clipstates,allstates=$allstates ClipStateRaster_bigmem.sbatch
 sleep 1s
 
-sbatch --job-name=ClipNortheast --export=ALL,cdlyear=$year,region='Northeast',\
-clipstates=$clipstates,allstates=$allstates ClipStateRaster_bigmem.sbatch
-sleep 1s
+#sbatch --job-name=ClipNortheast --export=ALL,cdlyear=$year,region='Northeast',\
+#clipstates=$clipstates,allstates=$allstates ClipStateRaster_bigmem.sbatch
+#sleep 1s
 
-sbatch --job-name=ClipMidwest --export=ALL,cdlyear=$year,region='Midwest',\
-clipstates=$clipstates,allstates=$allstates ClipStateRaster_bigmem.sbatch
-sleep 1s
+#sbatch --job-name=ClipMidwest --export=ALL,cdlyear=$year,region='Midwest',\
+#clipstates=$clipstates,allstates=$allstates ClipStateRaster_bigmem.sbatch
+#sleep 1s
 
-sbatch --job-name=CLipWest --export=ALL,cdlyear=$year,region='West',\
-clipstates=$clipstates,allstates=$allstates ClipStateRaster_bigmem.sbatch
+#sbatch --job-name=CLipWest --export=ALL,cdlyear=$year,region='West',\
+#clipstates=$clipstates,allstates=$allstates ClipStateRaster_bigmem.sbatch
