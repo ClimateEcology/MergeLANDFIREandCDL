e9ba9700e8673d7547b17bb1388b856555f16160
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2021-12-29 12:50:21 -0500

    try importing by name of variable

--- curlgoogle.py
+++ curlgoogle.py
@@ -9,7 +9,7 @@ Modified from code by: Dan Ellis 2020
 
 '''
 import os,sys,json,time
-import google_keys * # py file defining 'client_id' and 'client_secret', do not version control!
+from google_keys import client_id,client_secret # py file defining 'client_id' and 'client_secret', do not version control!
 
 startTime = time.time()
 
