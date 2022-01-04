51c7b6db78fc1359187ccf55e516295952be999c
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2022-01-04 16:06:30 -0500

    run only one test job

--- RunRegions.sh
+++ RunRegions.sh
@@ -13,9 +13,9 @@ sbatch --job-name=MergeNortheast --export=ALL,cdlyear=$year,region='Northeast',\
 mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
 sleep 1s
 
-sbatch --job-name=MergeMidwest --export=ALL,cdlyear=$year,region='Midwest',\
-mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
-sleep 1s
+# sbatch --job-name=MergeMidwest --export=ALL,cdlyear=$year,region='Midwest',\
+# mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
+# sleep 1s
 
-sbatch --job-name=MergeWest --export=ALL,cdlyear=$year,region='West',\
-mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
+# sbatch --job-name=MergeWest --export=ALL,cdlyear=$year,region='West',\
+# mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
