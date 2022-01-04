38e36fa2768b69c943e561728106a123bd4f49e8
melaniekamm <melanie.a.kammerer@gmail.com>
2022-01-04 17:00:27 -0600

    undo for loop in shell script. is this the problem?

--- RunRegions.sh
+++ RunRegions.sh
@@ -5,8 +5,9 @@ merge=TRUE
 allstates=TRUE
 
 # for year in 2012 2013 2014 2015 2016 2017 2018 2019 2020
-for year in 2016 2017
-do
+#for year in 2016 2017
+
+year=2016
 # sbatch --job-name=MergeSoutheast --export=ALL,cdlyear=$year,region='Southeast',\
 # mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
 # sleep 1s
@@ -21,4 +22,4 @@ sleep 1s
 
 # sbatch --job-name=MergeWest --export=ALL,cdlyear=$year,region='West',\
 # mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
-done
+
