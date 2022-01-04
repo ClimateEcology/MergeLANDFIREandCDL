946ba6460e6ef97e090944aae75f7c55834cf0d2
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2022-01-04 17:04:38 -0500

    update run regions to re-do all regions for 2 years

--- RunRegions.sh
+++ RunRegions.sh
@@ -2,12 +2,14 @@
 
 tiles=TRUE
 merge=TRUE
-allstates=FALSE
+allstates=TRUE
 
-year=2017
-# sbatch --job-name=MergeSoutheast --export=ALL,cdlyear=$year,region='Southeast',\
-# mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
-# sleep 1s
+# for year in 2012 2013 2014 2015 2016 2017 2018 2019 2020
+for year in 2016 2017
+do
+sbatch --job-name=MergeSoutheast --export=ALL,cdlyear=$year,region='Southeast',\
+mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
+sleep 1s
 
 sbatch --job-name=MergeNortheast --export=ALL,cdlyear=$year,region='Northeast',\
 mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
@@ -19,3 +21,4 @@ sleep 1s
 
 sbatch --job-name=MergeWest --export=ALL,cdlyear=$year,region='West',\
 mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
+done
