476617c4a3fe73641ffdf7f22fca547b1b77dbfa
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2022-01-12 15:56:04 -0500

    file size test

---
+++ code/functions/RunCheckRasterSize.R
@@ -0,0 +1,13 @@
+
+source('./code/functions/check_rastersize.R')
+
+alltiledirs <- list.dirs('../../../90daydata/geoecoservices/MergeLANDFIREandCDL', recursive=F)
+
+# use this file path if running from 90daydata
+#alltiledirs <- list.dirs(getwd(), recursive=F)
+
+alltiledirs <- alltiledirs[grepl(alltiledirs, pattern="Tiles")]
+
+for (onedir in alltiledirs) {
+  check_rastersize(onedir, cutoff_pct=0.85)
+}
--- translate_na_gdal.sh
+++ translate_na_gdal.sh
@@ -3,6 +3,9 @@
 
 gdalinfo D:/SpatialData/NASS_CDL/CDL2019/2019_30m_cdls.img
 
-gdal_calc -A D:/SpatialData/NASS_CDL/ZippedCDL/2019_30m_cdls/2019_30m_cdls.img --outfile=D:/SpatialData/NASS_CDL/ZippedCDL/2019_30m_cdls/2019_30m_cdls_fixNA.img --calc="A" --NoDataValue=0
+gdal_calc -A D:/SpatialData/NASS_CDL/CDL2017/2017_30m_cdls.img --outfile=D:/SpatialData/NASS_CDL/CDL2017/2017_30m_cdls_fixNA.img --calc="A" --NoDataValue=0
+
+gdal_calc -A 2019_30m_cdls.img --outfile=2019_30m_cdls_fixNA.img --calc="A" --NoDataValue=0
+
 
 #gdal_translate D:/SpatialData/NASS_CDL/ZippedCDL/2019_30m_cdls/AL_output.tif D:/SpatialData/NASS_CDL/ZippedCDL/2019_30m_cdls/AL_output2.tif -a_nodata -1001
