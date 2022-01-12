d9db2426fbd5fbeea881458b9ba0f69519b36800
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2022-01-11 20:00:05 -0500

    some states are still halved. Re-run all years to try to fix this

--- RunRegions.sh
+++ RunRegions.sh
@@ -5,20 +5,20 @@ merge=TRUE
 allstates=TRUE
 
 #for year in 2012 2013 2014 2015 2016 2017 2018 2019 2020
-for year in 2014
+for year in 2012 2016 2017 2019
 
 do
-#sbatch --job-name="SouthE$year" --export=ALL,cdlyear=$year,region='Southeast',\
-#mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
-#sleep 1s
+sbatch --job-name="SouthE$year" --export=ALL,cdlyear=$year,region='Southeast',\
+mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
+sleep 1s
 
-#sbatch --job-name="NorthE$year" --export=ALL,cdlyear=$year,region='Northeast',\
-#mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
-#sleep 1s
+sbatch --job-name="NorthE$year" --export=ALL,cdlyear=$year,region='Northeast',\
+mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
+sleep 1s
 
-#sbatch --job-name="MidW$year" --export=ALL,cdlyear=$year,region='Midwest',\
-#mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
-#sleep 1s
+sbatch --job-name="MidW$year" --export=ALL,cdlyear=$year,region='Midwest',\
+mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
+sleep 1s
 
 sbatch --job-name="West$year" --export=ALL,cdlyear=$year,region='West',\
 mktiles=$tiles,runmerge=$merge,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
---
+++ code/functions/tab_merged_raster.R
@@ -0,0 +1,21 @@
+
+tab_merged_raster <- function(state_path) {
+  if (grepl(state_path, pattern='TX_East')|grepl(state_path, pattern='TX_West')) {
+    statename <- stringr::str_sub(state_path, start=-22, end=-16)
+  } else {
+    statename <- stringr::str_sub(state_path, start=-17, end=-16)
+  }
+  
+  year <- stringr::str_sub(state_path, start=-11, end=-8)
+  
+  one_raster <- terra::rast(state_path)
+  
+  raster_freq <- terra::freq(one_raster) %>%
+    data.frame() %>%
+    dplyr::mutate(State = statename,
+                  Year = year) %>%
+    dplyr::rename(RasterClass = value, NCells=count)
+  
+  logger::log_info(paste0("Finished ", statename, "."))
+  return(raster_freq)
+}
