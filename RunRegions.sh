baac1f7684d6582f2635d59d17e1d1ea4cbd1321
melaniekamm <melanie.a.kammerer@gmail.com>
2022-01-13 12:52:51 -0600

    only run one year of Texas mosaic

--- RunRegions.sh
+++ RunRegions.sh
@@ -2,20 +2,21 @@
 
 tiles=TRUE
 merge=TRUE
+mosaic=TRUE
 allstates=FALSE
 
 #for year in 2012 2013 2014 2015 2016 2017 2018 2019 2020
 #for year in 2012 2016 2017 2019
-for year in 2019
+for year in 2017 2016 2012
 
 do
 sbatch --job-name="SouthE$year" --export=ALL,cdlyear=$year,region='Southeast',\
 mktiles=$tiles,runmerge=$merge,mosaic=$mosaic,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
 sleep 1s
 
-sbatch --job-name="NorthE$year" --export=ALL,cdlyear=$year,region='Northeast',\
-mktiles=$tiles,runmerge=$merge,mosaic=$mosaic,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
-sleep 1s
+#sbatch --job-name="NorthE$year" --export=ALL,cdlyear=$year,region='Northeast',\
+#mktiles=$tiles,runmerge=$merge,mosaic=$mosaic,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
+#sleep 1s
 
 sbatch --job-name="MidW$year" --export=ALL,cdlyear=$year,region='Midwest',\
 mktiles=$tiles,runmerge=$merge,mosaic=$mosaic,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
--- RunTexas.sh
+++ RunTexas.sh
@@ -5,8 +5,8 @@ merge=FALSE
 mosaic=TRUE
 allstates=FALSE
 
-for year in 2013 2014 2015 2018 2020
-
+#for year in 2013 2014 2015 2018 2020
+for year in 2020
 do
 sbatch --job-name=Texas$year --export=ALL,cdlyear=$year,region='Southeast',\
 mktiles=$tiles,runmerge=$merge,mosaic=$mosaic,allstates=$allstates RunTexas.sbatch
