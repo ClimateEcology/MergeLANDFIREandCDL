35a00674fde352d40c4016eb8211981127ece751
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2022-01-18 11:59:18 -0500

    try *just* mosaicking Texas mega-tiles (n=4) with two methods

--- RunTexas.sh
+++ RunTexas.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-tiles=FALSE
-merge=FALSE
-mosaic=TRUE
-allstates=FALSE
-
 #for year in 2013 2014 2015 2018 2020
 for year in 2020
 do
-sbatch --job-name=Texas$year --export=ALL,cdlyear=$year,region='Southeast',\
-mktiles=$tiles,runmerge=$merge,mosaic=$mosaic,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
+sbatch --job-name=MosaicTexas$year --export=ALL,terra=TRUE,gdal=FALSE RunTexas.sbatch
 sleep 1s
 
+sbatch --job-name=MosaicTexas$year --export=ALL,terra=FALSE,gdal=TRUE RunTexas.sbatch
+sleep 1s
 done
---
+++ code/functions/mosaicWTexas.R
@@ -0,0 +1,47 @@
+
+library(terra); library(gdalUtils)
+
+args <- commandArgs(trailingOnly = T)
+terra <- args[2] # year of NASS Cropland Data Layer
+gdal <- args[3] # region to process
+  
+# input parameters
+CDLYear <- 2020
+tiledir = outdir = '/90daydata/geoecoservices/MergeLANDFIREandCDL/TX_West_414/MergedCDLNVC'
+ID <- paste0('TX_West_CDL', CDLYear,'NVC')
+
+mega_paths <- list.files(tiledir, full.names=T)
+
+# exclude any extra files
+mega_paths <- mega_paths[!grepl(mega_paths, pattern= ".tif.aux")]
+mega_paths <- mega_paths[grepl(mega_paths, pattern= "MegaTile")]
+mega_paths <- mega_paths[!grepl(mega_paths, pattern= "MegaMega")]
+
+
+if (!is.na(ID)) {
+  mega_paths <- mega_paths[grepl(mega_paths, pattern=ID)]
+}
+
+
+mega_list <- vector("list", length(mega_paths))
+
+for (i in 1:length(mega_paths)) {
+  mega_list[[i]] <- terra::rast(mega_paths[i])
+}
+
+if (terra == T) {
+  a <- Sys.time()
+  base::eval(rlang::call2("mosaic", !!!get('mega_list'), .ns="terra", fun='mean', filename=paste0(tiledir, '/', ID, '_FinalRaster_terra.tif'), overwrite=T))
+  b <- Sys.time()
+  
+  logger::log_info(paste0(b-a, ' seconds to execute terra mosaic.'))
+}
+
+if (gdalUtils == T) {
+  c <- Sys.time()
+  gdalUtils::mosaic_rasters(gdalfile=mega_paths, dst_dataset=paste0(tiledir, '/', ID,'_FinalRaster_gdal.tif'))
+  d <- Sys.time()
+  
+  logger::log_info(paste0(d-c, ' seconds to execute gdalUtils mosaic.'))
+}
+
