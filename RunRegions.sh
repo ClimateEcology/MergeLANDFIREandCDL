27b425163da98f6d8b7a8bcd4475954edc976648
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2022-01-18 17:59:58 -0500

    something

--- RunRegions.sh
+++ RunRegions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-tiles=FALSE
-merge=FALSE
+tiles=TRUE
+merge=TRUE
 mosaic=TRUE
-allstates=FALSE
+allstates=TRUE
 
 #for year in 2012 2013 2014 2015 2016 2017 2018 2019 2020
 for year in 2020
