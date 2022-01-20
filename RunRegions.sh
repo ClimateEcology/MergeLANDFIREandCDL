793be1f74dd26f52709ec9a5f86c0e740a51d817
melaniekamm <melanie.a.kammerer@gmail.com>
2022-01-20 12:50:41 -0600

    atlas changes

--- RunRegions.sh
+++ RunRegions.sh
@@ -3,26 +3,26 @@
 tiles=TRUE
 merge=TRUE
 mosaic=TRUE
-allstates=FALSE
+allstates=TRUE
 
 #for year in 2012 2013 2014 2015 2016 2017 2018 2019 2020
-for year in 2020
+for year in 2012 2013
 
 do
 sbatch --job-name="SouthE$year" --export=ALL,cdlyear=$year,region='Southeast',\
 mktiles=$tiles,runmerge=$merge,mosaic=$mosaic,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
 sleep 1s
 
-#sbatch --job-name="NorthE$year" --export=ALL,cdlyear=$year,region='Northeast',\
+sbatch --job-name="NorthE$year" --export=ALL,cdlyear=$year,region='Northeast',\
+mktiles=$tiles,runmerge=$merge,mosaic=$mosaic,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
+sleep 1s
+
+sbatch --job-name="MidW$year" --export=ALL,cdlyear=$year,region='Midwest',\
+mktiles=$tiles,runmerge=$merge,mosaic=$mosaic,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
+sleep 1s
+
+#sbatch --job-name="West$year" --export=ALL,cdlyear=$year,region='West',\
 #mktiles=$tiles,runmerge=$merge,mosaic=$mosaic,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
 #sleep 1s
-
-# sbatch --job-name="MidW$year" --export=ALL,cdlyear=$year,region='Midwest',\
-# mktiles=$tiles,runmerge=$merge,mosaic=$mosaic,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
-# sleep 1s
-
-# sbatch --job-name="West$year" --export=ALL,cdlyear=$year,region='West',\
-# mktiles=$tiles,runmerge=$merge,mosaic=$mosaic,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch
-# sleep 1s
 
 done
