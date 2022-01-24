f20ab5d41fbf676b9b4b4922dcd18c817306ee95
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2022-01-24 15:20:21 -0500

    run tier 2 2019

--- pull_container_to_atlas.sh
+++ pull_container_to_atlas.sh
@@ -6,8 +6,8 @@ module load singularity/3.5.2
 singularity cache clean
 
 # pull desired version of container
-singularity pull docker://melaniekamm/geospatial_extend:v1.1
+singularity pull docker://melaniekamm/geospatial_extend:v1.2
 
 # rename container on Atlas to match all sbatch scripts
 rm geospatial_extend_latest.sif # remove old version of container (if there is one)
-cp geospatial_extend_v1.1.sif geospatial_extend_latest.sif # create updated container with same name
+cp geospatial_extend_v1.2.sif geospatial_extend_latest.sif # create updated container with same name
