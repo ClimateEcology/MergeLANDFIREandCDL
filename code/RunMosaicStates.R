33b2bc54bfdf72a03d356434e9cb5d47de39d136
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2022-01-24 18:46:41 -0500

    play with bash arguements

--- code/RunMosaicStates.R
+++ code/RunMosaicStates.R
@@ -7,11 +7,12 @@ args <- commandArgs(trailingOnly = T)
 CDLYear <- args[2] # year of NASS Cropland Data Layer
 tier <- args[3] # which hierarchy of mosaic states to process
 
+message(tier)
 #outdir <- 'D:/MergeLANDFIRECDL_Rasters/2017MergeCDL_LANDFIRE/' #file path on laptop
 
-outdir <- '../../../90daydata/geoecoservices/MergeLANDFIREandCDL/'
-statedir <- paste0(outdir,'/StateRasters/', CDLYear)
-ID <- paste0('CDL', CDLYear,'NVC')
-
-mosaic_states(outdir=outdir, statedir=statedir, ID=ID, tier=c(1), usepackage='gdal')
+# outdir <- '../../../90daydata/geoecoservices/MergeLANDFIREandCDL/'
+# statedir <- paste0(outdir,'/StateRasters/', CDLYear)
+# ID <- paste0('CDL', CDLYear,'NVC')
+# 
+# mosaic_states(outdir=outdir, statedir=statedir, ID=ID, tier=c(1), usepackage='gdal')
 
