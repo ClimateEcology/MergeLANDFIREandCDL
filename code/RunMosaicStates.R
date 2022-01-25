70bf1ff526a1bc87563b89b5ede25299eeeab38d
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2022-01-24 19:12:33 -0500

    use tier arguement instead of specifying in R

--- code/RunMosaicStates.R
+++ code/RunMosaicStates.R
@@ -8,12 +8,12 @@ message(args)
 CDLYear <- args[2] # year of NASS Cropland Data Layer
 tier <- unlist(stringr::str_split(args[3], pattern=":")) # which hierarchy of mosaic states to process
 
-message(tier)
+logger::log_info(tier)
 #outdir <- 'D:/MergeLANDFIRECDL_Rasters/2017MergeCDL_LANDFIRE/' #file path on laptop
 
-# outdir <- '../../../90daydata/geoecoservices/MergeLANDFIREandCDL/'
-# statedir <- paste0(outdir,'/StateRasters/', CDLYear)
-# ID <- paste0('CDL', CDLYear,'NVC')
-# 
-# mosaic_states(outdir=outdir, statedir=statedir, ID=ID, tier=c(1,2,3), usepackage='gdal')
+outdir <- '../../../90daydata/geoecoservices/MergeLANDFIREandCDL/'
+statedir <- paste0(outdir,'/StateRasters/', CDLYear)
+ID <- paste0('CDL', CDLYear,'NVC')
+
+mosaic_states(outdir=outdir, statedir=statedir, ID=ID, tier=tier, usepackage='gdal')
 
