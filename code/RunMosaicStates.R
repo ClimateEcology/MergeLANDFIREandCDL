fd29b95ec5d9ac8698b620adeff482f3b2e64c23
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2022-01-24 19:16:16 -0500

    fix glue?

--- code/RunMosaicStates.R
+++ code/RunMosaicStates.R
@@ -8,7 +8,7 @@ message(args)
 CDLYear <- args[2] # year of NASS Cropland Data Layer
 tier <- unlist(stringr::str_split(args[3], pattern=":")) # which hierarchy of mosaic states to process
 
-logger::log_info(tier)
+message(tier)
 #outdir <- 'D:/MergeLANDFIRECDL_Rasters/2017MergeCDL_LANDFIRE/' #file path on laptop
 
 outdir <- '../../../90daydata/geoecoservices/MergeLANDFIREandCDL/'
