8fe5a97738e42231389181952bdf6beee0bdf07a
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2022-02-04 16:02:23 -0500

    save list of job ids

--- learn_dependency.sh
+++ learn_dependency.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# Get first job id
+# declare empty string for all job ids
+jobids=""
 
+# Get first job id
 jid=$(sbatch old_sbatch_bash/hello_node_batch.sbatch | cut -d ' ' -f4)
 
 # Remainder jobs
 for k in {2..4};
     do 
-        jid=$(sbatch --dependency=afterok:${jid} old_sbatch_bash/hello_node_batch.sbatch | cut -d ' ' -f4)
+        #jid=$(sbatch --dependency=afterok:${jid} old_sbatch_bash/hello_node_batch.sbatch | cut -d ' ' -f4)
+        jid=$(sbatch old_sbatch_bash/hello_node_batch.sbatch | cut -d ' ' -f4)
+
+        jobids="$jobids, $jid"
     done
+echo $jobids    
