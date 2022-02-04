b65dfe1058a5db91eae0c0577433ae9547d620ac
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2022-02-04 16:17:56 -0500

    use dependencies with run regions bash script

--- RunRegionsTest.sh
+++ RunRegionsTest.sh
@@ -13,22 +13,22 @@ for year in 2020 2019 2018 2017 2016 2015 2014 2013 2012
 do
     seid=$(sbatch --job-name="SouthE$year" --export=ALL,cdlyear=$year,region='Southeast',\
     mktiles=$tiles,runmerge=$merge,mosaic=$mosaic,allstates=$allstates \
-    RunMerge_in_container_bigmem_bystate.sbatch | cut -d ' ' -f4)
+    old_sbatch_bash/hello_node_batch.sbatch | cut -d ' ' -f4)
     sleep 1s
 
     neid=$(sbatch --job-name="NorthE$year" --export=ALL,cdlyear=$year,region='Northeast',\
     mktiles=$tiles,runmerge=$merge,mosaic=$mosaic,allstates=$allstates \
-    RunMerge_in_container_bigmem_bystate.sbatch | cut -d ' ' -f4)
+    old_sbatch_bash/hello_node_batch.sbatch | cut -d ' ' -f4)
     sleep 1s
 
     mwid=$(sbatch --job-name="MidW$year" --export=ALL,cdlyear=$year,region='Midwest',\
     mktiles=$tiles,runmerge=$merge,mosaic=$mosaic,allstates=$allstates \
-    RunMerge_in_container_bigmem_bystate.sbatch | cut -d ' ' -f4)
+    old_sbatch_bash/hello_node_batch.sbatch | cut -d ' ' -f4)
     sleep 1s
 
     wid=$(sbatch --job-name="West$year" --export=ALL,cdlyear=$year,region='West',\
     mktiles=$tiles,runmerge=$merge,mosaic=$mosaic,allstates=$allstates \
-    RunMerge_in_container_bigmem_bystate.sbatch | cut -d ' ' -f4)
+    old_sbatch_bash/hello_node_batch.sbatch | cut -d ' ' -f4)
     sleep 1s
 
     jobids="$jobids,$seid,$neid,$mwid,$wid"
@@ -36,3 +36,6 @@ done
 
 jobids="${jobids:1}" # strip off leading comma
 echo $jobids  
+
+sbatch --dependency=afterok:${jobids} old_sbatch_bash/hello_node_batch.sbatch
+
