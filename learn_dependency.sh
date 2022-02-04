db72e07ea1736644f8aeed003c7c1303fe3fcc4e
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2022-02-04 15:48:03 -0500

    add sleep option to tell if dependency is working

--- learn_dependency.sh
+++ learn_dependency.sh
@@ -5,7 +5,7 @@
 jid=$(sbatch old_sbatch_bash/hello_node_batch.sbatch | cut -d ' ' -f4)
 
 # Remainder jobs
-for k in {2..10};
+for k in {2..4};
     do 
         jid=$(sbatch --dependency=afterok:${jid} old_sbatch_bash/hello_node_batch.sbatch | cut -d ' ' -f4)
     done
