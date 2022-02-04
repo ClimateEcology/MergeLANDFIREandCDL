2255eb6672cd86eb0683f2faab38aa1b0287a35c
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2022-02-04 16:39:55 -0500

    combine most run scripts in one bash script with dependencies

--- RunRegions.sh
+++ RunRegions.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+########## Part 1: Generate merged rasters
+# parameters for generating rasters
 tiles=TRUE
 merge=TRUE
 mosaic=TRUE
 allstates=TRUE
+jobids="" # declare empty string for all job ids (generate rasters)
 
-# declare empty string for all job ids
-jobids=""
 
 for year in 2020 2019 2018 2017 2016 2015 2014 2013 2012
 
@@ -35,4 +36,48 @@ do
 done
 
 jobids="${jobids:1}" # strip off leading comma
-echo $jobids  
+
+
+########## Part 2: Technical Validation
+# after all regions and years are finished (generating rasters), compile technical validation data
+sbatch --dependency=afterok:${jobids} TechnicalValidation.sbatch
+
+
+########## Part 3: Clip state rasters
+# parameters for generating rasters
+clipstates=TRUE
+clipids="" # declare empty string for all job ids (clip)
+
+for year in 2020 2019 2018 2017 2016 2015 2014 2013 2012
+
+do
+    seid2=$(sbatch --dependency=afterok:${jobids} --job-name="ClipSE$year" --export=ALL,cdlyear=$year,region='Southeast',\
+    clipstates=$clipstates,allstates=$allstates ClipStateRaster_bigmem.sbatch | cut -d ' ' -f4)
+    sleep 1s
+
+    neid2=$(sbatch --dependency=afterok:${jobids} --job-name="ClipNE$year" --export=ALL,cdlyear=$year,region='Northeast',\
+    clipstates=$clipstates,allstates=$allstates ClipStateRaster_bigmem.sbatch | cut -d ' ' -f4)
+    sleep 1s
+
+    mwid2=$(sbatch --dependency=afterok:${jobids} --job-name="ClipMW$year" --export=ALL,cdlyear=$year,region='Midwest',\
+    clipstates=$clipstates,allstates=$allstates ClipStateRaster_bigmem.sbatch | cut -d ' ' -f4)
+    sleep 1s
+
+    wid2=$(sbatch --dependency=afterok:${jobids} --job-name="ClipWest$year" --export=ALL,cdlyear=$year,region='West',\
+    clipstates=$clipstates,allstates=$allstates ClipStateRaster_bigmem.sbatch | cut -d ' ' -f4)
+    
+    clipids="$clipids,$seid2,$neid2,$mwid2,$wid2"
+done
+clipids="${clipids:1}" # strip off leading comma
+
+
+########## Part 4: Mosaic clipped rasters to national
+# parameters for mosaic
+tier='1:2:3'
+
+for year in 2020 2019 2018 2017 2016 2015 2014 2013 2012
+
+do
+    sbatch --dependency=afterok:${clipids} --job-name="Mosaic$year" --export=ALL,cdlyear="$year",tier="$tier" MosaicToNational.sbatch
+    sleep 1s
+done
