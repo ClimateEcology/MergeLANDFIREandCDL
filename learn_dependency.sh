254b8443f1acfb03058db0a8676cf62d076b9784
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2022-02-04 16:07:43 -0500

    run job after all previous are finished

--- learn_dependency.sh
+++ learn_dependency.sh
@@ -9,10 +9,11 @@ jid=$(sbatch old_sbatch_bash/hello_node_batch.sbatch | cut -d ' ' -f4)
 # Remainder jobs
 for k in {2..4};
     do 
-        #jid=$(sbatch --dependency=afterok:${jid} old_sbatch_bash/hello_node_batch.sbatch | cut -d ' ' -f4)
         jid=$(sbatch old_sbatch_bash/hello_node_batch.sbatch | cut -d ' ' -f4)
 
         jobids="$jobids,$jid"
     done
 jobids="${jobids:1}" # strip off leading comma
-echo $jobids    
+echo $jobids  
+
+sbatch --dependency=afterok:${jobids} old_sbatch_bash/hello_node_batch.sbatch
