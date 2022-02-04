de42575ce2f1205894e8bd4fc954187fa0991ed0
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2022-02-04 17:47:02 -0500

    change dependency to start after termination instead of start after successful terminate

--- RunRegions.sh
+++ RunRegions.sh
@@ -8,8 +8,9 @@ mosaic=TRUE
 allstates=TRUE
 jobids="" # declare empty string for all job ids (generate rasters)
 
+years=(2020 2019 2018 2017 2016 2015 2014 2013 2012)
 
-for year in 2020 2019 2018 2017 2016 2015 2014 2013 2012
+for year in $years
 
 do
     seid=$(sbatch --job-name="SouthE$year" --export=ALL,cdlyear=$year,region='Southeast',mktiles=$tiles,runmerge=$merge,mosaic=$mosaic,allstates=$allstates RunMerge_in_container_bigmem_bystate.sbatch | cut -d ' ' -f4)
@@ -31,7 +32,7 @@ jobids="${jobids:1}" # strip off leading comma
 
 ########## Part 2: Technical Validation
 # after all regions and years are finished (generating rasters), compile technical validation data
-sbatch --dependency=afterok:${jobids} TechnicalValidation.sbatch
+sbatch --dependency=afterany:${jobids} TechnicalValidation.sbatch
 
 
 ########## Part 3: Clip state rasters
@@ -39,19 +40,19 @@ sbatch --dependency=afterok:${jobids} TechnicalValidation.sbatch
 clipstates=TRUE
 clipids="" # declare empty string for all job ids (clip)
 
-for year in 2020 2019 2018 2017 2016 2015 2014 2013 2012
+for year in $years
 
 do
-    seid2=$(sbatch --dependency=afterok:${jobids} --job-name="ClipSE$year" --export=ALL,cdlyear=$year,region='Southeast',clipstates=$clipstates,allstates=$allstates ClipStateRaster_bigmem.sbatch | cut -d ' ' -f4)
+    seid2=$(sbatch --dependency=afterany:${jobids} --job-name="ClipSE$year" --export=ALL,cdlyear=$year,region='Southeast',clipstates=$clipstates,allstates=$allstates ClipStateRaster_bigmem.sbatch | cut -d ' ' -f4)
     sleep 1s
 
-    neid2=$(sbatch --dependency=afterok:${jobids} --job-name="ClipNE$year" --export=ALL,cdlyear=$year,region='Northeast',clipstates=$clipstates,allstates=$allstates ClipStateRaster_bigmem.sbatch | cut -d ' ' -f4)
+    neid2=$(sbatch --dependency=afterany:${jobids} --job-name="ClipNE$year" --export=ALL,cdlyear=$year,region='Northeast',clipstates=$clipstates,allstates=$allstates ClipStateRaster_bigmem.sbatch | cut -d ' ' -f4)
     sleep 1s
 
-    mwid2=$(sbatch --dependency=afterok:${jobids} --job-name="ClipMW$year" --export=ALL,cdlyear=$year,region='Midwest',clipstates=$clipstates,allstates=$allstates ClipStateRaster_bigmem.sbatch | cut -d ' ' -f4)
+    mwid2=$(sbatch --dependency=afterany:${jobids} --job-name="ClipMW$year" --export=ALL,cdlyear=$year,region='Midwest',clipstates=$clipstates,allstates=$allstates ClipStateRaster_bigmem.sbatch | cut -d ' ' -f4)
     sleep 1s
 
-    wid2=$(sbatch --dependency=afterok:${jobids} --job-name="ClipWest$year" --export=ALL,cdlyear=$year,region='West',clipstates=$clipstates,allstates=$allstates ClipStateRaster_bigmem.sbatch | cut -d ' ' -f4)
+    wid2=$(sbatch --dependency=afterany:${jobids} --job-name="ClipWest$year" --export=ALL,cdlyear=$year,region='West',clipstates=$clipstates,allstates=$allstates ClipStateRaster_bigmem.sbatch | cut -d ' ' -f4)
     
     clipids="$clipids,$seid2,$neid2,$mwid2,$wid2"
 done
@@ -62,9 +63,9 @@ clipids="${clipids:1}" # strip off leading comma
 # parameters for mosaic
 tier='1:2:3'
 
-for year in 2020 2019 2018 2017 2016 2015 2014 2013 2012
+for year in $years
 
 do
-    sbatch --dependency=afterok:${clipids} --job-name="Mosaic$year" --export=ALL,cdlyear="$year",tier="$tier" MosaicToNational.sbatch
+    sbatch --dependency=afterany:${clipids} --job-name="Mosaic$year" --export=ALL,cdlyear="$year",tier="$tier" MosaicToNational.sbatch
     sleep 1s
 done
