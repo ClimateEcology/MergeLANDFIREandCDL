c0a771ae1158406f28239c25fa79b30aaa3fa6dc
Melanie Kammerer <melanie.a.kammerer@gmail.com>
2022-02-09 17:20:40 -0500

    run tests doesn't depend on convert raster

--- RunPostProcessChecks.sh
+++ RunPostProcessChecks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 jobids="" # declare empty string for all job ids (converting data type)
-years=(2013) # which years to run
+years=(2014) # which years to run
 
 ########## Part 1: convert data type to Int16 for final, national rasters
 # run this section first, only start other jobs when these are complete
@@ -12,16 +12,16 @@ do
     sleep 1s
 
     jobids="$jobids,$convert_id"
+
+    ########## Part 2: Test that output rasters look good
+    # this sections runs checks on crs, extent, file size, and raster values
+    #year=all
+
+    sbatch --job-name="Tests$year" --export=ALL,cdlyear="$year" RunTests.sbatch
+    sleep 1s
+
 done
 jobids="${jobids:1}" # strip off leading comma
-
-########## Part 2: Test that output rasters look good
-# this sections runs checks on crs, extent, file size, and raster values
-#year=all
-year=2013
-
-sbatch --dependency=afterany:${jobids} --job-name="Tests$year" --export=ALL,cdlyear="$year" RunTests.sbatch
-sleep 1s
 
 ########## Part 3: Tabulate the number of pixels in NVC, CDL, and merged raster layers
 # for year in "${years[@]}"
